e802fc8e87493eb4eae5b84207b3dd81dd9ccbf1
ShanazarDev <hshanazar29@mail.ru>
2023-03-04 22:06:24 +0500

    Nothing to edit

---
+++ Decrypt.py
@@ -0,0 +1,124 @@
+# Import all from Encrypt.py file
+# from Encrypt import *
+from colorama import Fore
+
+
+# HexEncrypting class heir
+# If you wont to heir class from Encrypt.py file put your class Name to Decrypt class
+class Decrypt():
+    def __init__(self):
+        self.row_input = []
+        self.numbers = []
+        self.result = []
+        self.abc = {1: 'a', 2: 'b', 3: 'c', 4: 'd', 5: 'e', 6: 'f', 7: 'g', 8: 'h', 9: 'i', 10: 'j', 11: 'k', 12: 'l',
+                    13: 'm', 14: 'n', 15: 'o', 16: 'p', 17: 'q', 18: 'r', 19: 's', 20: 't', 21: 'u', 22: 'v', 23: 'w',
+                    24: 'x', 25: 'y', 26: 'z', 27: ' '}
+
+        print("Select language (type number of language): \t\n 1. Russian \t\n 2. English")
+        self.lang_input = input('\t')
+
+        if self.lang_input == '1':
+            for i in range(1, 17):
+                self.get_input = input(Fore.BLUE + 'Напишите свой HEX код по одной нажимая ввод: ')
+                if self.get_input == ' ':
+                    print(Fore.RED + 'Пустая строка не является доступом. Попробуйте еще раз!' + Fore.RESET)
+                    break
+                else:
+                    pass
+                self.row_input.append(self.get_input)
+            print(Fore.YELLOW + 'Ваш HEX код: \t', self.row_input)
+
+        elif self.lang_input == '2':
+            for i in range(1, 17):
+                self.get_input = input(Fore.BLUE + 'Enter 16 digits of HEX code 1 to 1 with pressing enter: ')
+                if self.get_input == ' ':
+                    print(Fore.RED + 'Empty string is not access. Try again!' + Fore.RESET)
+                    break
+                else:
+                    pass
+                self.row_input.append(self.get_input)
+            print(Fore.YELLOW + 'Your HEX code: \t', self.row_input)
+
+        else:
+            print(Fore.RED + 'Wrong input! Try again.\t\nНерпавильное значение! Попробуйте заново!')
+
+    # Checker input function Rus and Eng
+    def CheckerRus(self):
+        row_input = self.row_input
+
+        for i in row_input:
+            if i in ' ':
+                print(Fore.RED + 'Пустая строка не является доступом. Пожалуйста, попробуйте еще раз!' + Fore.RESET)
+                row_input.clear()
+            else:
+                pass
+
+        for i in row_input:
+            if 2 < len(i):
+                print(Fore.RED + 'Ошибка. Длина одной цифры не должна превышать 2 (Длина цифр 2!)' + Fore.RESET)
+                row_input.clear()
+            else:
+                pass
+
+    def CheckerEng(self):
+        row_input = self.row_input
+
+        for i in row_input:
+            if i in ' ':
+                print(Fore.RED + 'Empty string is not access. Please try again!' + Fore.RESET)
+                row_input.clear()
+            else:
+                pass
+
+        for i in row_input:
+            if 2 < len(i):
+                print(Fore.RED + 'Error. Length of one digits should not exceed (Digits length 2!)' + Fore.RESET)
+                row_input.clear()
+            else:
+                pass
+
+    # Decrypting inputted HEX code
+    def Decrypting(self):
+
+        # Alphabetical numbers of hex code list
+        numbers = self.numbers
+        # Finish result list
+        result = self.result
+
+        for i in self.row_input:
+            r = int(i, 16)
+            numbers.append(r)
+
+        if self.lang_input == '1':
+            print(Fore.LIGHTGREEN_EX + 'Нумерация в алфавите : ', numbers)
+        elif self.lang_input == '2':
+            print(Fore.LIGHTGREEN_EX + 'Alphabetical numbering :) : ', numbers)
+
+        # Getting value from dictionary of ABC
+        def get_val():
+            for i in numbers:
+                if i in self.abc:
+                    res = self.abc.get(i)
+                    result.append(res)
+
+        get_val()
+        one_line = ''.join([''.join(sub) for sub in result])
+
+        if self.lang_input == '1':
+            print(Fore.LIGHTGREEN_EX + 'Ваша зашифрованная строка расшифрована :) : \t' + Fore.RED + one_line)
+        elif self.lang_input == '2':
+            print(Fore.LIGHTGREEN_EX + 'Your encrypted string decrypted :) : \t' + Fore.RED + one_line)
+
+    # Language selector
+    def Lang(self):
+        if self.lang_input == '1':
+            dec.CheckerRus()
+            dec.Decrypting()
+
+        elif self.lang_input == '2':
+            dec.CheckerEng()
+            dec.Decrypting()
+
+
+dec = Decrypt()
+dec.Lang()
---
+++ Encrypt.py
@@ -0,0 +1,174 @@
+from colorama import Fore
+
+# class AES, all algorithm
+class HexEncrypting:
+    # Inputs function
+    def __init__(self):
+        print("Select language (type number of language): \t\n 1. Russian \t\n 2. English")
+        self.lang = input('\t')
+
+        if self.lang == '1':
+            self.row_input = input(Fore.BLUE + 'Введите слово для шифрования в HEX (длина строки должна быть 16): ')
+
+        elif self.lang == '2':
+            self.row_input = input(Fore.BLUE + 'Enter your word for encrypting to HEX (string length should be 16): ')
+        else:
+            print(Fore.RED + 'Wrong input! Try again.\t\nНерпавильное значение! Попробуйте заново!')
+
+        self.rowed_input = []
+        self.new_key = []
+        self.row_ABC = []
+        self.HEX_code = []
+        self.HEC_line = []
+        self.abc = {1: 'a', 2: 'b', 3: 'c', 4: 'd', 5: 'e', 6: 'f', 7: 'g', 8: 'h', 9: 'i', 10: 'j', 11: 'k', 12: 'l', 13: 'm', 14: 'n', 15: 'o', 16: 'p', 17: 'q', 18: 'r', 19: 's', 20: 't', 21: 'u', 22: 'v', 23: 'w', 24: 'x', 25: 'y', 26: 'z', 27: ' '}
+        self.rowed_input.append(self.row_input.lower())
+
+        for i in self.row_input:
+            if i in str(list(range(1, 11))):
+                if self.lang == '1':
+                    print(Fore.RED + 'Только буквы, попробуйте еще раз!!!' + Fore.RESET)
+                elif self.lang == '2':
+                    print(Fore.RED + 'Only letters, try again!!!' + Fore.RESET)
+            else:
+                break
+
+
+    # Scan input for validate
+    def CheckingRus(self):
+        if len(self.row_input) > 16:
+            print(Fore.YELLOW + 'длина строки больше 16!' + Fore.RESET)
+            self.row_input = input(Fore.BLUE + 'Попробуйте еще раз: ')
+            self.rowed_input.clear()
+            self.rowed_input.append(self.row_input.lower())
+
+        elif len(self.row_input) < 16:
+            print(Fore.YELLOW + 'Ваша набранная строка настолько мала, что нужно 16 букв!' + Fore.RESET)
+            self.row_input = input(Fore.BLUE + 'Попробуйте еще раз: ')
+            self.rowed_input.clear()
+            self.rowed_input.append(self.row_input.lower())
+
+        else:
+            print(Fore.RED + f'Принято: {self.rowed_input}' + Fore.RESET)
+
+    def CheckingEng(self):
+        if len(self.row_input) > 16:
+            print(Fore.YELLOW + 'string length 16!'+ Fore.RESET)
+            self.row_input = input(Fore.BLUE + 'Try again: ')
+            self.rowed_input.clear()
+            self.rowed_input.append(self.row_input.lower())
+
+        elif len(self.row_input) < 16:
+            print(Fore.YELLOW + 'Your typed string so small need 16 letters!' + Fore.RESET)
+            self.row_input = input(Fore.BLUE + 'Try again: ')
+            self.rowed_input.clear()
+            self.rowed_input.append(self.row_input.lower())
+
+        else:
+            print(Fore.RED + f'Accept: {self.rowed_input}' + Fore.RESET)
+
+    # Encrypt information
+    def EncryptRus(self):
+        b = str(self.rowed_input)
+        d = b.strip("[]'")
+
+        for i in d:
+            self.new_key.append(i)
+
+        # Get KEY form dictionaries
+        def get_key(val):
+            for KEY, value in self.abc.items():
+                if val == value:
+                    return KEY
+
+        # Set what KEY need
+        for x in self.new_key:
+            if x in self.new_key:
+                key = get_key(x)
+                self.row_ABC.append(key)
+            # Empty string
+            elif x is None in self.new_key:
+                key = get_key(x)
+                self.row_ABC.append(key)
+
+        print(Fore.MAGENTA + f'Алфавитный номер: {self.row_ABC}')
+
+        for i in self.row_ABC:
+            HEX = hex(i).upper()
+            res = HEX.strip('0X')
+            self.HEX_code.append(res)
+
+        print(
+            Fore.LIGHTGREEN_EX + f'HEX зашифрованное слово: {self.rowed_input} \t\n' + Fore.RESET + Fore.BLUE + 'Зашифрованная таблица HEX 4x4: ',end='')
+
+        # 4x4 table HEX code
+        for i in range(1):
+            print(Fore.MAGENTA)
+            print(f'1 строка ---> {self.HEX_code[:4]}')
+            print(f'2 строка ---> {self.HEX_code[4:8]}')
+            print(f'3 строка ---> {self.HEX_code[8:12]}')
+            print(f'4 строка ---> {self.HEX_code[12:16]}')
+
+        # One line string
+        one_line = ' '.join([''.join(sub) for sub in self.HEX_code])
+        self.HEC_line.append(one_line)
+        print()
+        print(Fore.BLUE + f'HEX без 4x4: {self.HEC_line}')
+
+    def EncryptEng(self):
+        b = str(self.rowed_input)
+        d = b.strip("[]'")
+
+        for i in d:
+            self.new_key.append(i)
+
+        # Get KEY form dictionaries
+        def get_key(val):
+            for KEY, value in self.abc.items():
+                if val == value:
+                    return KEY
+        # Set what KEY need
+        for x in self.new_key:
+            if x in self.new_key:
+                key = get_key(x)
+                self.row_ABC.append(key)
+            # Empty string
+            elif x is None in self.new_key:
+                key = get_key(x)
+                self.row_ABC.append(key)
+
+        print(Fore.MAGENTA + f'Alphabetical letter number: {self.row_ABC}')
+
+        for i in self.row_ABC:
+            HEX = hex(i).upper()
+            res = HEX.strip('0X')
+            self.HEX_code.append(res)
+
+        print(Fore.LIGHTGREEN_EX + f'HEX encrypted word: {self.rowed_input} \t\n' + Fore.RESET + Fore.BLUE + 'Encrypted HEX 4x4 table: ', end='')
+
+        # 4x4 table HEX code
+        for i in range(1):
+            print(Fore.MAGENTA)
+            print(f'1 string ---> {self.HEX_code[:4]}')
+            print(f'2 string ---> {self.HEX_code[4:8]}')
+            print(f'3 string ---> {self.HEX_code[8:12]}')
+            print(f'4 string ---> {self.HEX_code[12:16]}')
+
+        # One line string
+        one_line  = ' '.join([''.join(sub) for sub in self.HEX_code])
+        self.HEC_line.append(one_line)
+        print()
+        print(Fore.BLUE + f'HEX without 4x4: {self.HEC_line}')
+
+    def Language(self):
+        if self.lang == '1':
+            h.CheckingRus()
+            h.EncryptRus()
+
+        elif self.lang == '2':
+            h.CheckingEng()
+            h.EncryptEng()
+
+# Run
+if __name__ == '__main__':
+    h = HexEncrypting()
+    h.Language()
